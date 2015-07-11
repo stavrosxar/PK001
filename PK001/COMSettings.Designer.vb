36e548479639b1fde7194a7a7985c9db024f6e41
stavrosxar <stavrosxar@gmail.com>
2015-07-11 15:01:39 +0200

    ComSettingsPage

--- PK001/COMSettings.Designer.vb
+++ PK001/COMSettings.Designer.vb
@@ -23,6 +23,7 @@ Partial Class COMSettings
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.COMSelect = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'COMSelect
@@ -33,16 +34,28 @@ Partial Class COMSettings
         Me.COMSelect.Size = New System.Drawing.Size(121, 21)
         Me.COMSelect.TabIndex = 1
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(49, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
         'COMSettings
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(449, 303)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.COMSelect)
         Me.Name = "COMSettings"
         Me.Text = "COMSettings"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents COMSelect As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 End Class
